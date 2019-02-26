64e3d1769941adfd85dc6ea8545400f2a22c2a73
naomijoyce <naomijoyceb@gmail.com>
2019-02-25 19:42:23 -0500

    setting up store and reducers

--- src/index.js
+++ src/index.js
@@ -2,17 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import { Provider } from "react-redux";
 import * as serviceWorker from './serviceWorker';
 
-// const store = createStore()
+import designReducer from "./reducers/designReducer";
+import eventReducer from "./reducers/eventReducer";
+import inviteReducer from "./reducers/inviteReducer";
+import userReducer from "./reducers/userReducer";
+
+const rootReducer = combineReducers({
+  designInfo: designReducer,
+  eventInfo: eventReducer, 
+  inviteInfo: inviteReducer,
+  userInfo: userReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
     <App />
-  </BrowserRouter>, 
+  </BrowserRouter>
+  </Provider>, 
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
