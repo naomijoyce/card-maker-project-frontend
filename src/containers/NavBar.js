e35900fffd77a9867594c7939a06fac5648632af
naomijoyce <naomijoyceb@gmail.com>
2019-02-28 17:51:32 -0500

    got some protected routes- little success

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const NavBar = () => {
+const NavBar = ({loggedIn}) => {  
   return (
     <div className="nav-bar">
       <ul>
         <Link to="/"><li>Home</li></Link>
-        <Link to="/designeditor"><li>Create a Design</li></Link>
-        <Link to="/mydesigns"><li>My Designs</li></Link> 
-        <Link to="/myinvites"><li>My Invites</li></Link>
-        <Link to="/account"><li>Account Settings</li></Link>
-        <Link to="/loginorsignup"><li>Log In/Sign Up</li></Link>
+
+        {
+          loggedIn ? 
+          (
+            <div>
+              <Link to="/designeditor"><li>Create a Design</li></Link>
+              <Link to="/mydesigns"><li>My Designs</li></Link> 
+              <Link to="/myinvites"><li>My Invites</li></Link>
+              <Link to="/account"><li>Account Settings</li></Link>
+            </div>
+          ):
+          <Link to="/loginorsignup"><li>Log In/Sign Up</li></Link>
+        }
+        
+
+        
       </ul>
     </div>
   );
 };
 
-export default NavBar;
+const mapStateToProps = (state) => {
+  return{
+    loggedIn: state.userInfo.loggedIn
+  }
+}
+
+export default connect(mapStateToProps)(NavBar);
